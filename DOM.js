138799cbd8842dd55db35a00c95cfdbaa4d03126
Mavya-555 <mavyak010@gmail.com>
2024-10-25 21:09:15 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,138 @@
+// ===== TASK-1 =====
+// let p = document.createElement("p");
+// p.innerText = "Welcome to JavaScript DOM manipulation";
+// document.body.appendChild(p);
+
+// ===== TASK-2 =====
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1");
+// h1.innerText = "Main Heading";
+// let h2 = document.createElement("h2");
+// h2.innerText = "Subheading";
+// div.append(h1,h2);
+// document.body.appendChild(div);
+
+// ===== TASK-3 =====
+// let ul = document.createElement("ul");
+// let li1 = document.createElement("li");
+// li1.innerText = "list - 1";
+// let li2 = document.createElement("li");
+// li2.innerText = "list - 2";
+// let li3 = document.createElement("li");
+// li3.innerText = "list - 3";
+// ul.append(li1,li2,li3);
+// document.body.append(ul);
+
+// ===== TASK-4 =====
+// let table = document.createElement("table");
+// table.className = "tableN";
+
+// let tr1 = document.createElement("tr");
+// let th11 = document.createElement("th");
+// th11.innerText = "S.No";
+// let th12 = document.createElement("th");
+// th12.innerText = "PRODUCT";
+// let th13 = document.createElement("th");
+// th13.innerText = "PRICE";
+// tr1.append(th11,th12,th13);
+
+// let tr2 = document.createElement("tr");
+// let td21 = document.createElement("td");
+// td21.innerText = "1.";
+// let td22 = document.createElement("td");
+// td22.innerText = "Chocolate";
+// let td23 = document.createElement("td");
+// td23.innerText = "20/-";
+// tr2.append(td21,td22,td23);
+
+// let tr3 = document.createElement("tr");
+// let td31 = document.createElement("td");
+// td31.innerText = "2.";
+// let td32 = document.createElement("td");
+// td32.innerText = "cake";
+// let td33 = document.createElement("td");
+// td33.innerText = "30/-";
+// tr3.append(td31,td32,td33);
+
+// let tr4 = document.createElement("tr");
+// let td41 = document.createElement("td");
+// td41.innerText = "3.";
+// let td42 = document.createElement("td");
+// td42.innerText = "Juice";
+// let td43 = document.createElement("td");
+// td43.innerText = "50/-";
+// tr4.append(td41,td42,td43);
+
+// table.append(tr1,tr2,tr3,tr4);
+// document.body.appendChild(table);
+
+
+// ===== TASK-5 =====
+// let div = document.createElement("div");
+// let img = document.createElement("img");
+// let p = document.createElement("p");
+// div.className = "container";
+// img.src = "cat-img.jpg";
+// img.className = "img";
+// img.alt = "img missing";
+// p.innerText = "Hello Ketty";
+// div.append(img,p);
+// document.body.appendChild(div);
+
+// ===== TASK-6 =====
+// let ol1 = document.createElement("ol");
+// let ol2 = document.createElement("ol");
+// let li1 = document.createElement("li");
+// li1.innerText = "list - 1";
+// let li2 = document.createElement("li");
+// li2.innerText = "list - 2";
+// let li3 = document.createElement("li");
+// li3.innerText = "list - 3";
+// let li4 = document.createElement("li");
+// li4.innerText = "list - 2.1";
+// let li5 = document.createElement("li");
+// li5.innerText = "list - 2.2";
+// ol2.append(li4,li5);
+// li2.appendChild(ol2);
+// ol1.append(li1,li2,li3);
+// document.body.appendChild(ol1)
+
+// ===== TASK-7 =====
+// let section = document.createElement("section");
+// let h2 = document.createElement("h2");
+// let p = document.createElement("p");
+// h2.innerText = "Header tag";
+// p.innerText = "paragraph tag";
+// section.append(h2,p);
+// document.body.appendChild(section)
+
+// =====TASK-8 =====
+// let dt1 = document.createElement("dt");
+// dt1.innerText = "Discription List - 1";
+// let dd1 = document.createElement("dd");
+// dd1.innerText = "Discription Data - 1";
+// dt1.appendChild(dd1);
+// let dt2 = document.createElement("dt");
+// dt2.innerText = "Discription List - 2";
+// let dd2 = document.createElement("dd");
+// dd2.innerText = "Discription Data - 2";
+// dt2.appendChild(dd2);
+// document.body.append(dt1,dt2)
+
+// ===== TASK-9 =====
+// let div = document.createElement("div");
+// let blockquote = document.createElement("blockquote");
+// blockquote.innerText = "blockquote tag";
+// div.appendChild(blockquote);
+// document.body.appendChild(div);
+
+// ===== TASK - 10 =====
+// let div = document.createElement("div");
+// let p1 = document.createElement("p");
+// p1.innerText = "Paragraph - 1";
+// let p2 = document.createElement("p");
+// p2.innerText = "Paragraph - 2";
+// let p3 = document.createElement("p");
+// p3.innerText = "Paragraph - 3";
+// div.append(p1,p2,p3);
+// document.body.append(div);
